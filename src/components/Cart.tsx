66a2ca12e8d12d7fe88d71a568d6ad90b46f77f8
NguyenQuangTrinh <nguyenquangtrinh01102323@gmail.com>
2023-12-21 13:29:05 +0700

    delete

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdDelete } from "react-icons/md";
+import axios from 'axios';
+import { getAccessTokenFromCookie } from './cookies';
+import { useDispatch } from 'react-redux';
+import { deletePostById } from '../redux/feature/post';
+import toast from 'react-hot-toast';
 
 interface CardProps {
   name: string;
@@ -12,28 +18,58 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ name, status, url, datapublish, id, uli }) => {
 
- 
+  const dispatch = useDispatch();
+
+  function deletePost() {
+    console.log(id);
+    console.log(uli.split('/')[1]);
+    const deletes = axios.delete(`https://www.googleapis.com/blogger/v3/blogs/${uli.split('/')[1]}/posts/${id}`, {
+      headers: { Authorization: `Bearer  ${getAccessTokenFromCookie()}` }
+    })
+      .then(res => {
+        dispatch(deletePostById({id: id}))
+      })
+      .catch(e => {
+        console.log(e);
+      })
+
+      toast.promise(
+        deletes,
+        {
+          loading: 'Deleting...',
+          success: <b>Deleted</b>,
+          error: <b>Delete Error.</b>,
+        }
+      )
+  }
+
   return (
     <div className="max-w-md bg-white shadow-lg rounded-md overflow-hidden mx-auto m-4 hover:shadow-2xl">
-    <NavLink to={`/${uli}/${id}`}>
-      <div className="p-4">
-        {/* Tên */}
-        <h2 className="text-xl font-semibold mb-2">{name}</h2>
+      <NavLink to={`/${uli}/${id}`}>
+        <div className="p-4">
+          {/* Tên */}
+          <h2 className="text-xl font-semibold mb-2">{name}</h2>
 
-        {/* Trạng thái */}
-        <p className="text-gray-600 mb-2">Status: {status}</p>
+          {/* Trạng thái */}
+          <p className="text-gray-600 mb-2">Status: {status}</p>
 
-        {/* URL */}
-        <p className="text-blue-500 mb-2">
-          <span rel="noopener noreferrer">
-            {url}
-          </span>
-        </p>
+          {/* URL */}
+          <p className="text-blue-500 mb-2">
+            <span rel="noopener noreferrer">
+              {url}
+            </span>
+          </p>
 
-        {/* Ngày xuất bản */}
-        <p className="text-gray-500">Published on: {datapublish}</p>
-      </div>
-    </NavLink>
+          {/* Ngày xuất bản */}
+          <p className="text-gray-500">Published on: {datapublish}</p>
+
+        </div>
+      </NavLink>
+      {
+        uli !== "listPost" && <div className='p-3'>
+          <MdDelete onClick={() => deletePost()} className="w-5 h-5" />
+        </div>
+      }
     </div>
   );
 };
--- src/redux/feature/post.ts
+++ src/redux/feature/post.ts
@@ -18,11 +18,17 @@ export const postSlice = createSlice({
     getPost: (state, actions: PayloadAction<Post>) => {
       state.value = actions.payload != undefined ? actions.payload : null
     },
-  
+    deletePostById: (state, actions: PayloadAction<any>) => {
+      const index = state.value?.items.findIndex((items) => items.post_user_info.postId == actions.payload.id);
+      console.log(index);
+      if (index != -1) {
+        state.value?.items.splice(index, 1);
+      }
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { getPost } = postSlice.actions
+export const { getPost, deletePostById } = postSlice.actions
 
 export default postSlice.reducer
